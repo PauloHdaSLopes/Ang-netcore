6c3273d2b68a9b96f66fba87fa66ae448a9df80c
Paulo Lopes <paulohdas.lopes@gmail.com>
2019-02-03 10:32:14 -0200

    End of section 8

--- Course.API/Helpers/Extensions.cs
+++ Course.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace Course.API.Helpers
@@ -9,7 +10,16 @@ namespace Course.API.Helpers
             response.Headers.Add("Application-Error",message);
             response.Headers.Add("Access-Controle-Expose-Headers","Applicatin-Error");
             response.Headers.Add("Access-Controle-Allow-Origin","*");
+        }
 
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+
+            return age;
         }
     }
 }
--- Course.API/Startup.cs
+++ Course.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Course.API.Data;
 using Course.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -33,9 +34,14 @@ namespace Course.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>( x=> x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(Options=> 
+                    Options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddCors();
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository,AuthRepository>();
+            services.AddScoped<IDatingRepository,DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -50,7 +56,7 @@ namespace Course.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -74,6 +80,7 @@ namespace Course.API
                 });
             }
 
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
